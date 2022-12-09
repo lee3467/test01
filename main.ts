0976f7378834911f5fb18a0f0ee544d97b16d2f3
이재윤 <jyun0807@naver.com>
2022-12-09 11:22:39 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    RingbitCar.freestyle(0, 0)
-    RingbitCar.freestyle(0, 0)
+    RingbitCar.running_time(RingbitCar.Direction_run.forward, 3)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 3)
+    RingbitCar.brake()
 })
 input.onButtonPressed(Button.B, function () {
     RingbitCar.brake()
