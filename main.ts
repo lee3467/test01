7f8abc38db8155c66f111a56e4dabc8f276f148b
이재윤 <jyun0807@naver.com>
2022-12-16 14:40:17 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     RingbitCar.running_time(RingbitCar.Direction_run.forward, 4.3)
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 150)
-    RingbitCar.running_time(RingbitCar.Direction_run.forward, 5.5)
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 240)
-    RingbitCar.running_time(RingbitCar.Direction_run.forward, 3.5)
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 120)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 190)
+    RingbitCar.running_time(RingbitCar.Direction_run.forward, 6.8)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 250)
+    RingbitCar.running_time(RingbitCar.Direction_run.forward, 4)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 115)
 })
 input.onButtonPressed(Button.B, function () {
     RingbitCar.brake()
